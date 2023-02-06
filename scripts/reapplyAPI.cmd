4ce183748e78d9cc88c292098eaa796963dda65b
AlphaKR93 <alphakr93@gmail.com>
2023-02-06 18:38:37 +0900

    Scripts

--- scripts/reapplyAPI.cmd
+++ scripts/reapplyAPI.cmd
@@ -1,5 +1,5 @@
 @echo off
 taskkill /f /t /im java.exe >> NUL
 taskkill /f /t /im git.exe >> NUL
-rd /s /q Andromeda-API >> NUL
+rd /s /q AndromedaPlus-API >> NUL
 call gradlew.bat applyAPIPatches
--- scripts/reapplyServer.cmd
+++ scripts/reapplyServer.cmd
@@ -1,5 +1,5 @@
 @echo off
 taskkill /f /t /im java.exe >> NUL
 taskkill /f /t /im git.exe >> NUL
-rd /s /q Andromeda-Server >> NUL
+rd /s /q AndromedaPlus-Server >> NUL
 call gradlew.bat applyServerPatches
